6f133a2940b067078ba24100bdafc42001357bc8
neul0129 <neul0129@naver.com>
2022-01-12 18:07:30 +0900

    menu수정
    18:07

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,18 +3,21 @@ import './Menu.css';
 
 const Menu = () => {
     return (
-        <div className='background_center'>
-            <h2 className='menu_title'>SHOP SINGAPORE'S FINEST TEA SELECTIONS</h2>
-            <ul className='menu_box'>
-                <li className='menu_item'><img src='./images/1.png'></img></li>
-                <li className='menu_item'><img src='./images/2.png'></img></li>
-                <li className='menu_item'><img src='./images/3.png'></img></li>
-                <li className='menu_item'><img src='./images/4.png'></img></li>
-                <li className='menu_item'><img src='./images/5.png'></img></li>
-                <li className='menu_item'><img src='./images/6.png'></img></li>
-            </ul>
+      <div className='background_center'>
+        <h2 className='menu_title'>SHOP SINGAPORE'S FINEST TEA SELECTIONS</h2>
+        <div className='menu_box'>
+          <div className='menu_item'>
+            <img src='./images/1.png'></img>
+            <div></div>  
+          </div>
+          <div className='menu_item'><img src='./images/2.png'></img></div>
+          <div className='menu_item'><img src='./images/3.png'></img></div>
+          <div className='menu_item'><img src='./images/4.png'></img></div>
+          <div className='menu_item'><img src='./images/5.png'></img></div>
+          <div className='menu_item'><img src='./images/6.png'></img></div>
         </div>
+      </div>
     );
 };
 
-export default Menu;
+export default Menu;    
