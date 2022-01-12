6b0521cf15a351e8d7e0bf3b452adb4062cc85be
neul0129 <neul0129@naver.com>
2022-01-12 16:00:39 +0900

    홈, 네브바 추가
    홈, 네브바 추가

---
+++ src/components/Home.js
@@ -0,0 +1,58 @@
+import './Home.css';
+import { Link } from 'react-router-dom';
+
+function Home(){
+  return(
+    <div>
+
+      {/* 시작 이미지 */}
+      <div className='wording'>
+        <div className='free-shipping'>
+          Now Shipping Globally!
+        </div>
+        <img className="background" alt='bg-img' src="./images/teapod.jpg"></img>
+        <Link to="/"><div className='background-btn'>Shop Now</div></Link>
+      </div>
+
+      {/* 카드 앞 설명 */}
+      <div className='card-def'>
+        <h1>exclusively handcrafted teas</h1>
+        <h4>he point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters
+          <br /> as opposed to using 'Content here, content here'</h4>
+      </div>
+
+      {/* 카드 형식 */}
+      <div className='home_card-container'>
+        <div className='home_card'>
+          <div className='card_blend'>classic blends</div>
+          <img src='./images/1.png' className='card-img' alt='about_image'></img>
+          <div className='card_title'>Chamomile</div>
+          <button className='card-btn'>add to cart</button>
+        </div>
+        <div className='home_card'>
+          <div className='card_blend'>classic blends</div>
+          <img src='./images/2.png' className='card-img' alt='about_image'></img>
+          <div className='card_title'>Rooibos</div>
+          <button className='card-btn'>add to cart</button>
+        </div>
+        <div className='home_card'>
+          <div className='card_blend'>classic blends</div>
+          <img src='./images/3.png' className='card-img' alt='about_image'></img>
+          <div className='card_title'>Earl Grey</div>
+          <button className='card-btn'>add to cart</button>
+        </div>
+      </div>
+
+      {/* 리뷰 */}
+      <div className='testimonial'>
+        <img src='./images/testimonial-1.jpg' alt='about_image'></img>
+        <img src='./images/testimonial-2.jpg' alt='about_image'></img>
+        <img src='./images/testimonial-3.jpg' alt='about_image'></img>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Home;
