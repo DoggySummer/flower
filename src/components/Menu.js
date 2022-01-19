0ca563a2281f6eae16aae731061f84e3968b7315
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2022-01-19 19:19:50 +0900

    map함수 적용

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import { React, useState } from 'react';
 import './Menu.css';
 
+
+
 const Menu = () => {
+	let [tea, setTea] = useState(['루이보스', '얼그레이', '민트']);
+
     return (
 			<div className='background_img'>
 				<div className='menu_background_center'>
 					<h1 className='menu_title'>SHOP SINGAPORE'S FINEST TEA SELECTIONS</h1>
 					<div className='menu_box'>
-						<div className='menu_item'>
-							<img src='./images/1.png' className='img_front'></img>
-							<img src='./images/5.png' className='img_back'></img>
-							<h3>first product</h3>	
-							<div className='AddToCart'>Add to Cart</div>  
-						</div>
+
+			{tea.map(function(a,i){
+			return (<div className='menu_item' key="{ i }">
+				<img src='./images/1.png' className='img_front'></img>
+				<img src='./images/5.png' className='img_back'></img>
+				<h3>{ a }</h3>	
+				<div className='AddToCart'>Add to Cart</div> 
+			</div>)
+			})
+}
 					</div>
 				</div>
 			</div>
     );
-};
+					
+		};
 
 export default Menu;    
