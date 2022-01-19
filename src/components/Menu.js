37e85aab4fe28ea27925875deab9ece7ed7591cd
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2022-01-19 22:30:04 +0900

    map 1차 커밋

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,26 +1,41 @@
 import { React, useState } from 'react';
 import './Menu.css';
-
+import data from './data/data.json';
 
 
 const Menu = () => {
 	let [tea, setTea] = useState(['루이보스', '얼그레이', '민트']);
-
+	{console.log(data);}
     return (
 			<div className='background_img'>
 				<div className='menu_background_center'>
 					<h1 className='menu_title'>SHOP SINGAPORE'S FINEST TEA SELECTIONS</h1>
 					<div className='menu_box'>
 
-			{tea.map(function(a,i){
-			return (<div className='menu_item' key="{ i }">
+					{data.data.map(function(item,i){
+					<div className='menu_item' key={ { i } }>
+					{/* <img src={item[i].front_image} className='img_front' />
+					<img src={item[i].back_image} className='img_back' /> */}
+					<h3>{ item[i].title }</h3>	
+					<p>{ item[i].price }</p>	
+					<div className='AddToCart'>Add to Cart</div> 
+					</div>
+				})
+			}
+
+
+
+
+			{/* {tea.map(function(a, i){
+			return (
+			<div className='menu_item' key="{i}">
 				<img src='./images/1.png' className='img_front'></img>
 				<img src='./images/5.png' className='img_back'></img>
 				<h3>{ a }</h3>	
 				<div className='AddToCart'>Add to Cart</div> 
 			</div>)
-			})
-}
+			}) */}
+
 					</div>
 				</div>
 			</div>
