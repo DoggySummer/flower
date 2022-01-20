f8abea810b2fba702bc98dc2c25738e5318c49d1
DoggySummer <91588779+DoggySummer@users.noreply.github.com>
2022-01-20 13:04:17 +0900

    menu.css
    메뉴의 map 함수 미디어쿼리 정렬

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,7 +4,7 @@ import data from './data/data.json';
 
 
 const Menu = () => {
-	let [tea, setTea] = useState(['루이보스', '얼그레이', '민트']);
+	let [tea, setTea] = useState(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']);
 	{console.log(data);}
     return (
 			<div className='background_img'>
@@ -12,29 +12,20 @@ const Menu = () => {
 					<h1 className='menu_title'>SHOP SINGAPORE'S FINEST TEA SELECTIONS</h1>
 					<div className='menu_box'>
 
-					{data.data.map(function(item,i){
-					<div className='menu_item' key={ { i } }>
-					{/* <img src={item[i].front_image} className='img_front' />
-					<img src={item[i].back_image} className='img_back' /> */}
-					<h3>{ item[i].title }</h3>	
-					<p>{ item[i].price }</p>	
-					<div className='AddToCart'>Add to Cart</div> 
-					</div>
-				})
-			}
 
-
-
-
-			{/* {tea.map(function(a, i){
+			{tea.map(function(a, i){
 			return (
 			<div className='menu_item' key="{i}">
-				<img src='./images/1.png' className='img_front'></img>
-				<img src='./images/5.png' className='img_back'></img>
-				<h3>{ a }</h3>	
-				<div className='AddToCart'>Add to Cart</div> 
+				<div className='menu_img'>
+					<img src='./images/1.png' className='img_front'></img>
+					<img src='./images/5.png' className='img_back'></img>
+				</div>
+				<div className='meu_description'>
+					<h3>{ a }</h3>	
+					<div className='AddToCart'>Add to Cart</div> 
+				</div>
 			</div>)
-			}) */}
+			})}
 
 					</div>
 				</div>
