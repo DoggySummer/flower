1b04e3d5da40ed1b8c72f6bcc6478e3e4e359ac5
neul0129 <neul0129@naver.com>
2022-01-25 01:51:05 +0900

    navvar
    네브바 수정1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,37 +9,37 @@ const Navbar = (props) => {
   {console.log(window.outerWidth)}
   const menuToggle = () => {
     if(ulAppear == 1){
-    document.querySelector('.header').style.height = '265px';
-    document.querySelector('.header > ul').style.display = 'block';
-    setUlAppear(2);
-    }
-    else if(ulAppear == 2){
-      document.querySelector('.header').style.height = '100px';
-      document.querySelector('.header > ul').style.display = 'none';
-      setUlAppear(1);
-    }
-
-    if (window.matchMedia("max-width:901px")) {
-      console.log('big');
-    } else {
-      console.log('small');
-    }
+			document.querySelector('.header').style.height = '265px';
+			document.querySelector('.header > ul').style.display = 'block';
+			if (window.matchMedia >'901px') {
+				document.querySelector('.header > ul').style.display = 'flex';
+				document.querySelector('.header').style.height = '100px';
+			}
+			if (window.matchMedia < '900px') {
+				document.querySelector('.header > ul').style.display = 'block';
+				document.querySelector('.header').style.height = '265px';
+			}
+			setUlAppear(2);
+		}
+		else if(ulAppear == 2){
+			document.querySelector('.header').style.height = '100px';
+			document.querySelector('.header > ul').style.display = 'none';
+			setUlAppear(1);
+		}
   }
   const [ulAppear,setUlAppear] = useState(1);
-  const [showMenu, setShowMenu] = useState(true);
   return (
     <div>
       <header className='header'> 
         <div className='logo'>
 					<Link to="/" className='LinkHome'><img src='./images/SOF-logo.png' /></Link>
         </div>
-        {showMenu && <ul>
+        <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/About">About</Link></li>
           <li><Link to="/Menu">Menu</Link></li>
           <li><Link to="/Contact">Contact</Link></li>
         </ul>
-        }
         <div className="icons">
         <Link to="/Cart" className='icon_cart'>
           <button className='icon_1'>
