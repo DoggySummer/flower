3efaa5f76df7e04f6a79fc41eb0c5498e076fcdf
neul0129 <neul0129@naver.com>
2022-01-30 14:39:23 +0900

    menu
    menu.js redux적용

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,25 @@
 import { React, useState } from 'react';
 import './Menu.css';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 
 
-const Menu = (props) => {
-	
+const Menu = ([props]) => {
+		const state = useSelector(state => state);
 		return (
 			<div className='background_img'>
 				<div className='menu_background_center'>
 					<h1 className='menu_title'>SHOP SINGAPORE'S FINEST TEA SELECTIONS</h1>
 					<div className='menu_box'>
 
-			{props.state.map(function(a,i){
+			{state.map(function(value,i){
 			return (
 			<div className='menu_item' key="{i}">
 				<div className='menu_img'>
-					<img src={props.state[i].front_Image} className='img_front'></img>
-					<img src={props.state[i].back_Image} className='img_back'></img>
+					<img src={state[i].front_Image} className='img_front'></img>
+					<img src={state[i].back_Image} className='img_back'></img>
 				</div>
 				<div className='menu_description'>
-					<h3>{a.name}</h3>	
+					<h3>{value.name}</h3>	
 					<div className='AddToCart'onClick={()=>props.dispatch({type: `add_`+i})}>Add to Cart</div> 
 				</div>
 			</div>)
@@ -28,15 +28,7 @@ const Menu = (props) => {
 				</div>
 			</div>
     );
-					
-		};
+	};
 
-		function mapStateToProps(state){
-			return {
-				state : state
-			}
-		};
 		
-		export default connect(mapStateToProps)(Menu);
-
-		//onClick={()=>props.dispatch({type: `add_`+i})}
+		export default Menu;
